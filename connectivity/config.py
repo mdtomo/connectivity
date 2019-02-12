dcd6f353c23f9a7836d0104c3f3f62417e841fa2
Matthew Thomas <mdthomas@hotmail.ca>
2019-02-11 18:42:02 -0800

    Client and Server working correctly.

--- connectivity/config.py
+++ connectivity/config.py
@@ -5,16 +5,16 @@ from enum import Enum
 class Mode(Enum):
     CLIENT = 1
     SERVER = 2
-    CLIENT_SERVER = 3
 
 
 class Config:
 
     MODE = Mode.CLIENT
     LOCAL_ADDR = '127.0.0.1'
-    TARGET_ADDR = '127.0.0.1'
+    REMOTE_ADDR = '127.0.0.1'
     BEAT_SECS = 5
-    PORT = 20019
+    LOCAL_PORT = 2019
+    REMOTE_PORT = 2019
     LOG_LEVEL = 10 # DEBUG
     LOG_FORMAT = '%(asctime)s: %(levelname)s: %(module)s/%(funcName)s: %(message)s'
     SAVE_LOG = True
--- connectivity/connectivity.py
+++ connectivity/connectivity.py
@@ -12,18 +12,34 @@ class UDPServer:
     # https://docs.python.org/3/library/asyncio-protocol.html#asyncio-protocol
     def connection_made(self, transport):
         self.transport = transport
-        logger.debug(f'Connection made')
+        sock = transport.get_extra_info('socket')
+        if sock is not None:
+            addr = sock.getsockname()
+            logger.debug(f'Server listening at {addr[0]}:{addr[1]}')
 
     def datagram_received(self, data, addr):
         msg = data.decode()
-        logger.info(f'{addr} {msg}')
+        logger.info(f'Received from {addr[0]}:{addr[1]} {msg}')
+
+    def connection_lost(self, exc):
+        if exc is None:
+            logger.debug('Server connection closed.')
 
 
 class UDPClient:
 
     def connection_made(self, transport):
         self.transport = transport
-        transport.sendto('beat'.encode(), (Config.TARGET_ADDR, Config.PORT))
+        sock = transport.get_extra_info('socket')
+        if sock is not None:
+            laddr = sock.getsockname()
+            raddr = sock.getpeername()
+            logger.debug(f'Sending beat from {laddr[0]}:{laddr[1]} to {raddr[0]}:{raddr[1]}')
+        transport.sendto('beat'.encode(), (Config.REMOTE_ADDR, Config.REMOTE_PORT))
+        transport.close()
+
+    def connection_lost(self, exc):
+        pass
 
 
 async def create_server():
@@ -31,17 +47,24 @@ async def create_server():
     loop = asyncio.get_running_loop()
     transport, protocol = await loop.create_datagram_endpoint(
         lambda: UDPServer(),
-        local_addr=(Config.LOCAL_ADDR, Config.PORT)
+        local_addr=(Config.LOCAL_ADDR, Config.LOCAL_PORT)
     )
-    logger.debug(dir(transport))
-    logger.debug(protocol.__str__)
 
 
 async def beat_sender():
+    global transport
     while True:
         if transport is not None:
-            logger.debug('Sending beat...')
-            transport.sendto('beat'.encode(), (Config.TARGET_ADDR, Config.PORT))
+            # Use the already created UDP server socket to send beats.
+            logger.debug('Server sending beat...')
+            transport.sendto('beat'.encode(), (Config.REMOTE_ADDR, Config.REMOTE_PORT))
+        elif transport is None and Config.MODE is Mode.CLIENT:
+            # Create new connection to send beat to target server.
+            loop = asyncio.get_running_loop()
+            client_transport, protocol = await loop.create_datagram_endpoint(
+                lambda: UDPClient(),
+                remote_addr=(Config.REMOTE_ADDR, Config.REMOTE_PORT)
+    )
         await asyncio.sleep(Config.BEAT_SECS)
 
 
@@ -67,26 +90,16 @@ def main():
     loop = asyncio.get_event_loop()
     tasks = []
     if Config.MODE is Mode.CLIENT:
-        '''
-        Just send heart beats to target
-        '''
+        # Just send heart beats to target
         tasks.append(loop.create_task(beat_sender()))
     elif Config.MODE is Mode.SERVER:
-        '''
-        Just listen for heart beats from target
-        '''
+        # Just listen for heart beats.
         tasks.append(loop.create_task(create_server()))
-    if Config.MODE is Mode.CLIENT_SERVER:
-        '''
-        Listen for heart beats and send heart beats
-        '''
-        tasks.append(loop.create_task(create_server()))
-        tasks.append(loop.create_task(beat_sender()))
 
     all_tasks = asyncio.gather(*tasks)
-
     try:
         loop.run_until_complete(all_tasks)
+        loop.run_forever()
     except KeyboardInterrupt:
         logger.info('Program exiting...')
         logging.shutdown()
